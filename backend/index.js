6aa19b38ece4287e11148ccf0d25b65e3095fa8c
Kaue Oliveira <kaue.o@codium.co>
2021-05-26 09:45:50 +0700

    fix database access

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('../kamikaze.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./kamikaze.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Connected to the in-memory SQlite database.');
+    console.log('Connected to the SQLite database.');
 });
 
 db.serialize(() => {
-    db.each(` id,
-                  image
+    db.each(` SELECT photo,
+                  description
            FROM photos`, (err, row) => {
         if (err) {
             console.error(err.message);
         }
-        console.log(row.id + "\t" + row.name);
+        console.log(row.photo + "\t" + row.description);
     });
 });
 
