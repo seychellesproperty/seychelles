ed47ba3c9ff0d5be46249ff5a870daa1681e52cd
Kaue Oliveira <kaue.o@codium.co>
2021-05-30 12:54:10 +0700

    remove unused code

--- kamikaze/src/app/gallery.service.ts
+++ kamikaze/src/app/gallery.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -7,23 +7,8 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class GalleryService {
 
   constructor(private httpClient: HttpClient) { }
-  headers: HttpHeaders | undefined;
-  serverUrl = 'http://localhost:3000';
   fields = 'media_url,media_type,caption';
   tokenIG = '';
-
-  setHeader() {
-    this.headers = new HttpHeaders()
-      .set('Origin', 'http://localhost');
-  }
-
-  getImages() {
-    return this.httpClient.get(this.serverUrl + '/get-images', {headers: this.headers});
-  }
-
-  uploadImage(data: any) {
-    return this.httpClient.post(this.serverUrl + '/save-image', data,{headers: this.headers});
-  }
 
   loadPhotosFromInstagram() {
 
