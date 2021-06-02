ef7bb3dc9ad392e6030dfbf341eb9b664fdf5e5e
Kaue Oliveira <kaue.o@codium.co>
2021-06-02 22:38:27 +0700

    changes

--- kamikaze/src/app/description/description.component.ts
+++ kamikaze/src/app/description/description.component.ts
@@ -7,6 +7,7 @@ import {Router} from "@angular/router";
   styleUrls: ['./description.component.scss']
 })
 export class DescriptionComponent implements OnInit {
+  pdfIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   constructor(private router: Router) { }
 
